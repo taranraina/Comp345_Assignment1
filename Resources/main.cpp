3002b00b7eb6601cbf3865e5295ea04f8d5202fe
taran <taran.rainaa@gmail.com>
2020-02-09 01:25:08 -0500

    Initial Commit

---
+++ Resources/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Resources
+//
+//  Created by Taran Raina on 2020-02-09.
+//  Copyright © 2020 Taran Raina. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
