a343edefec93d27bb5c26219339f3e4e50d5d699
taran <taran.rainaa@gmail.com>
2020-02-09 20:42:51 -0500

    print tiles

--- Resources/main.cpp
+++ Resources/main.cpp
@@ -15,12 +15,11 @@ int main()
     
 }
 
-//number of resource per tile
-int HarvestTiles::numOfResourcePerTile(){
+int HarvestTiles::numOfResourcePerTile(){ //number of resource per tile
     int num_resources = 0;
     std::uniform_real_distribution<> dis(0.0, 1.0);
     double rand_val = dis(gen);
-    std::cout << rand_val << std::endl;  //just to see the val for debug purposes, remove
+    //std::cout << rand_val << std::endl;  //just to see the val for debug purposes, remove
     if (rand_val < 0.5) num_resources = 2;
     else num_resources = 3;
     return num_resources;
@@ -28,52 +27,46 @@ int HarvestTiles::numOfResourcePerTile(){
 
 HarvestTiles::HarvestTiles() {
     std::array<int, 4> resource_array{ 0,1,2,3 };
-    
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    std::shuffle(resource_array.begin(), resource_array.end(), std::default_random_engine(seed));
+    std::shuffle(resource_array.begin(), resource_array.end(), std::default_random_engine(seed)); //shuffles the array.
     
-    for (int i = 0; i < 4; i++) {
-        std::cout << "this is resource: " <<resource_array[i] << std::endl;
-    } //just for a print
-    
-    
-    //    Node res[2][2];
-    ht = new Node[4];
+    harvestTile = new Node[4];
     int i = 0;
     
-    //*(*(arr + i) + j))
     while (i < numOfResourcePerTile()) {
-        *(ht+(i)) = Node(Resource(resource_array[i]));
+        *(harvestTile+(i)) = Node(Resource(resource_array[i]));
         i++;
     }
+    std::uniform_int_distribution<> rand_roll(0, numOfResourcePerTile()-1);
     
-    for (int j = 0; j < 4; j++) {
-        std::cout << " this is the ht res " <<(ht+j)->getResourceName() << std::endl;
-        
-    }
-    //PROBLEM IS HERE
-    std::uniform_int_distribution<> rand_roll(0, num_resources-1);
-    
-    if (num_resources == 3) {
-        int rand = rand_roll(gen);
-        *(ht+3) = Node(Resource(resource_array[rand]));
+    if (numOfResourcePerTile() == 3) {
+        int lastResource = rand_roll(gen);
+        *(harvestTile+3) = Node(Resource(resource_array[lastResource]));
     }
     else {
-        int rand1 = rand_roll(gen);
-        *(ht+2) = Node(Resource(resource_array[rand1]));
-        int rand2 = rand_roll(gen);
-        *(ht+3) = Node(Resource(resource_array[rand2]));
+        int thirdResource = rand_roll(gen);
+        *(harvestTile+2) = Node(Resource(resource_array[thirdResource]));
+        int fourthResource = rand_roll(gen);
+        *(harvestTile+3) = Node(Resource(resource_array[fourthResource]));
     }
+    printTile();
     
-    //debug the ht array
+}
+void HarvestTiles::printTile(){
     for (int j = 0; j < 4; j++) {
-        std::cout << "this is the new ht res " <<(ht+j)->getResourceName() << std::endl;
+        if(j<2)
+            std::cout <<(harvestTile+j)->getResourceName()+ " " ;
+        else if(j==2){
+            std::cout <<"\n"+(harvestTile+j)->getResourceName()+ " "  ;
+        }
+        else
+            std::cout <<(harvestTile+j)->getResourceName()<<std::endl ;
     }
 }
 
 HarvestTiles::~HarvestTiles() {
-    delete[] ht;
-    ht = nullptr;
+    delete[] harvestTile;
+    harvestTile = nullptr;
 }
 
 void HarvestTiles::orientation(int degrees) {
@@ -91,13 +84,13 @@ Node::Node(Resource resource) {
 
 std::string Node::getResourceName(){
     switch (resource) {
-        case wheat  :  res="wheat"; break;
-        case wood:  res="wood"; break;
-        case sheep : res="sheep";  break;
-        case stone : res= "stone";  break;
+        case wheat  :  resourceName="wheat"; break;
+        case wood: resourceName="wood"; break;
+        case sheep : resourceName="sheep";  break;
+        case stone : resourceName= "stone";  break;
             
     }
-    return res;
+    return resourceName;
 }
 
 int Node:: getResourse(){
